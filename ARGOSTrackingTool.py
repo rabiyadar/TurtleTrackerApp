946ed5fa3c06bdbe0fded88ed10f3e9b9e69f673
rabiyadar <rabiya.dar@duke.edu>
2021-09-28 14:23:04 -0400

    Process lines using for loop

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -14,24 +14,26 @@ file_name = './data/raw/Sara.txt'
 #Create a file object from the file
 file_object = open(file_name,'r')
 
-#Read contents of file into a list
+#Read the first line of the file
 line_list = file_object.readlines()
 
 #Close the file
 file_object.close()
 
-#Pretend we read one line of data from the file
-lineString = line_list[101]
+#Iterate through all lines in lineList
+for lineString in line_list:
+    if lineString[0] in ("#", "u"):
+        continue
 
-#Split the string into a list of data items
-lineData = lineString.split()
-
-#Extract items in list into variables
-record_id = lineData[0]
-obs_date = lineData[2]
-obs_lc = lineData[4]
-obs_lat = lineData[6]
-obs_lon = lineData[7]
-
-#Print the location of sara
-print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+    #Split the string into a list of data items
+    lineData = lineString.split()
+    
+    #Extract items in list into variables
+    record_id = lineData[0]
+    obs_date = lineData[2]
+    obs_lc = lineData[4]
+    obs_lat = lineData[6]
+    obs_lon = lineData[7]
+    
+    #Print the location of sara
+    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
